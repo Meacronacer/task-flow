ed97175c245e6a96bae88cf509f188e70225e937
Ivan Popovych <sinnerscrown09@gmail.com>
2026-08-23 17:24:28 +0300

    feat: setup nestjs foundation with configmodule, typeorm, swagger

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,10 +1,37 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+        DATABASE_URL: Joi.string().required(),
+        REDIS_URL: Joi.string().required(),
+        JWT_ACCESS_SECRET: Joi.string().required(),
+        JWT_REFRESH_SECRET: Joi.string().required(),
+        JWT_ACCESS_EXPIRES: Joi.string().default('15m'),
+        JWT_REFRESH_EXPIRES: Joi.string().default('30d'),
+      }),
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.get('DATABASE_URL'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/migrations/*{.ts,.js}'],
+        synchronize: false,
+        logging: config.get('NODE_ENV') === 'development',
+      }),
+    }),
+  ],
 })
 export class AppModule {}
